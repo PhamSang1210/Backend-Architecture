490e9f5ecd32fa58cac50d64ddf714fc7b2eb14c
PhamSang1210 <phamhongsang12x10@gmail.com>
2023-11-13 16:35:38 +0700

    Halmet&Compression

--- src/app.js
+++ src/app.js
@@ -2,16 +2,19 @@ import express from "express";
 import helmet from "helmet";
 const app = express();
 import morgan from "morgan";
-
+import compression from "compression";
 //init middleware
-app.use(morgan("tiny"));
+app.use(morgan("dev"));
 app.use(helmet());
+app.use(compression());
 // init db
 
 // init routes
 app.get("/", (req, res, next) => {
+    const str = "compression";
     res.status(200).json({
         message: "Welcome Nodejs",
+        metaData: str.repeat(10000),
     });
 });
 // handleing error
